fb2af24f0c1e31425d7ca9112affae6ba465fdcb
Kamil Lapaj <kamillapajprogrammimng@gmail.com>
2021-09-20 09:43:12 +0200

    ''

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends_master.db'
 # initialize the database
 db = SQLAlchemy(app)
 # create db model
